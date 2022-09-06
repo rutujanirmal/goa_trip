3c40eee9e5dd8d4108ca52a35a9fd5ccc415692f
Pratham Goel <pratham.goel16@gmail.com>
2022-09-06 19:05:35 +0530

    Updated databse with 3 epd_ids. Update method complete.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,6 +6,26 @@ class EmployeesController < ApplicationController
   end
   def update
     # We will get the 3 emp_ids (r1,r2,r3). Update in the Employee Table
+    @ids = sortParams
+    x = 0
+    @ids.each_value do |id|
+      x += 1
+    end
+    if x != 3
+      render json: {"result": "Fail"}
+    else
+      @ids.each_value do |id|
+        obj = Employee.find_by(emp_id: id)
+        obj.allocated = true
+        obj.save!
+      end
+      render json: {"result": "Successfull"}
+    end
+  end
 
+  private
+
+  def sortParams
+    params.require(:ids).permit(:id1, :id2, :id3)
   end
 end
