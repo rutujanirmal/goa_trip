e05cbd1a3dd539505d51ec916f13caf218839aa8
Pratham Goel <pratham.goel16@gmail.com>
2022-09-06 18:01:46 +0530

    Set routes for get and puts requests to employee controller. index method is for get, update method is for put

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "getRequest", to: "employees#index" 
+
+  put "putReuqest", to: "employees#update"
+
 end
