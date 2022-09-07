a119de8d229f04a65fce88f5dc46967e18dac45c
Rutuja Nirmal <64598190+rutujanirmal@users.noreply.github.com>
2022-09-07 15:58:02 +0530

    Update Cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000','127.0.0.1:3000'
+    origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
