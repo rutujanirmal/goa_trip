668b865b15cf34801e555519e3463770536ce642
Pratham Goel <pratham.goel16@gmail.com>
2022-09-08 17:16:37 +0530

    Revert "checked for room mates gender. also added check if any one of the roomate allready allocated in a room"
    This reverts commit f0495c049f82b99969e1c8270ecb7e6c7483f7c5.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,28 +7,25 @@ class EmployeesController < ApplicationController
 
   def update
     # We will get the 3 emp_ids (r1,r2,r3). Update in the Employee Table
-    ids = sortParams.as_json
-    if ids.length != 3
+    ids = sortParams
+    x = 0
+    ids.each_value do |id|
+      x += 1
+    end
+    if x != 3
       render status: 400 , json: {error: "Fail due to number of arguments"}
     else
-      employees_data = Employee.select("full_name", "gender", "emp_id", "allocated").where("emp_id": [ids["id1"], ids["id2"], ids["id3"]]).as_json
-      if room_mates_of_diffrent_gender?(employees_data)
-        render status: 400, json: {error: "All three are not of same gender"}
-      elsif any_one_allocated?(employees_data)
-        render status: 400, json: {error: "any one of them is allocated"}
-      else
-        ids.each_value do |id|
-          obj = Employee.find_by(emp_id: id)
-          obj.allocated = true
-          obj.save!
-        end
-        obj = Room.new
-        obj.room_mate1, obj.room_mate2, obj.room_mate3 = ids["id1"], ids["id2"], ids["id3"]
-        obj.full_name = Employee.find_by(emp_id: ids["id1"]).as_json["full_name"]
-        obj.room_number = "A" + obj.room_mate1
+      ids.each_value do |id|
+        obj = Employee.find_by(emp_id: id)
+        obj.allocated = true
         obj.save!
-        render status: 200 , json: {result: "Successfull"}
       end
+      obj = Room.new
+      obj.room_mate1, obj.room_mate2, obj.room_mate3 = ids[:id1], ids[:id2], ids[:id3]
+      obj.full_name = Employee.find_by(emp_id: ids[:id1]).full_name
+      obj.room_number = "A" + obj.room_mate1
+      obj.save!
+      render status: 200 , json: {result: "Successful "}
     end
   end
 
@@ -48,17 +45,6 @@ class EmployeesController < ApplicationController
   end
 
   private
-
-  def any_one_allocated?(data)
-    return data[0]["allocated"] || data[1]["allocated"] || data[2]["allocated"]
-  end
-
-  def room_mates_of_diffrent_gender?(data)
-    if data[0]["gender"] == data[1]["gender"] && data[0]["gender"] == data[2]["gender"]
-      return false
-    end
-    true
-  end
 
   def sortParams
     params.require(:ids).permit(:id1, :id2, :id3)
