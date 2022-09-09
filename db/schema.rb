652227c83e35bbe05e885da2bcf27623e50bd17e
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-09 14:11:44 +0530

    Add room col to employees

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_103514) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_080128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_07_103514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "room"
     t.index ["emp_id"], name: "index_employees_on_emp_id", unique: true
   end
 
