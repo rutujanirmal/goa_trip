c56cd4362e4d20705c7f40e80b0128873c75f873
rutujanirmal <rutujanirmal8888@gmail.com>
2022-09-15 07:01:15 +0530

    Modify routes to access all the functions

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,26 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "employees", to: "employees#index" 
-
-  put "booking", to: "employees#update"
-
-  get "empdetails", to: "employees#fetch_details"
-
-  get "room_details", to: "employees#fetch_room_details"
-
+  Rails.application.routes.draw do
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+    # Defines the root path route ("/")
+    # root "articles#index"
+  
+    get "employees", to: "employees#index" 
+  
+    post "employees/new", to: "employees#create"
+  
+    post "booking", to: "employees#update"
+  
+    get "empdetails", to: "employees#fetch_details"
+  
+    get "room_details", to: "rooms#fetch_room_details"
+  
+    get "employees/pending", to: "employees#pending"
+  
+    delete "booking/delete", to: "rooms#destroy_room"
+  
+  end
+  
 end
